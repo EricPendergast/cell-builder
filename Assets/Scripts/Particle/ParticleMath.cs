f22be555840cd90696243819d71656f43fe2a5d3
epender <epender@umich.edu>
2021-01-16 11:24:03 -0500

    Slightly improve particle physics

--- Assets/Scripts/Particle/ParticleMath.cs
+++ Assets/Scripts/Particle/ParticleMath.cs
@@ -5,8 +5,13 @@ using Unity.Transforms;
 public class ParticleMath {
 
     public static void ResolveCollision(Translation t1, ref CollisionResponse c1, Translation t2) {
-        if (math.lengthsq(t1.Value - t2.Value) < .5) {
-            c1.deltaPosition += (t1.Value.xy - t2.Value.xy)*.5f;
+        
+        float radius = .5f;
+
+        var p2ToP1 = t1.Value.xy - t2.Value.xy;
+        float distance = math.length(p2ToP1);
+        if (distance < radius*2) {
+            c1.deltaPosition += math.normalizesafe(p2ToP1)*(radius*2-distance)/2;
         }
     }
 }
