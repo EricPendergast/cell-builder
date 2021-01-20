360a18f11d58e5f127512bc196d8c30d29bdf9ab
epender <epender@umich.edu>
2021-01-20 18:53:43 -0500

    Implement multi hash map iterators for burst

--- Assets/Scripts/Particle/MyParticleSystem.cs
+++ Assets/Scripts/Particle/MyParticleSystem.cs
@@ -47,21 +47,9 @@ public class MyParticleSystem : SystemBase {
 
                 int2 centerGrid = ToGrid(pos.Value.xy);
 
-                bool found = false;
-
-                for (int o_x = -1; o_x <= 1; o_x++) {
-                    for (int o_y = -1; o_y <= 1; o_y++) {
-                        int2 searchGrid = centerGrid + new int2(o_x, o_y);
-
-                        NativeMultiHashMapIterator<int> it;
-                        found = grid.TryGetFirstValue((int)math.hash(searchGrid), out int entityId, out it);
-                        while (found) {
-                            Translation otherPos = positions[entityId];
-                            ParticleMath.ResolveCollision(pos, ref collisionResponse, otherPos);
-
-                            found = grid.TryGetNextValue(out entityId, ref it);
-                        }
-                    }
+                foreach (int entityId in new NearbyCellIterator<int>(centerGrid, grid)) {
+                    Translation otherPos = positions[entityId];
+                    ParticleMath.ResolveCollision(pos, ref collisionResponse, otherPos);
                 }
 
             }).ScheduleParallel();
@@ -99,5 +87,69 @@ public class MyParticleSystem : SystemBase {
 
     private static int GridHash(float2 p) {
         return (int)math.hash(ToGrid(p));
+    }
+
+    private struct NearbyCellIterator<T> where T : struct {
+        private int2 centerGrid;
+        private NativeMultiHashMap<int, T> grid;
+        private MultiHashMapCellIterator<T> it;
+        private int o_x, o_y;
+        
+        public NearbyCellIterator<T> GetEnumerator() => this;
+
+        public NearbyCellIterator(int2 centerGrid, NativeMultiHashMap<int, T> grid) {
+            this.centerGrid = centerGrid;
+            this.grid = grid;
+            o_x = -1;
+            o_y = -1;
+            it = new MultiHashMapCellIterator<T>(grid, (int)math.hash(centerGrid + new int2(o_x, o_y)));
+        }
+    
+        public bool MoveNext() {
+            while (!it.MoveNext()) {
+                o_y++;
+                if (o_y > 1) {
+                    o_y = -1;
+                    o_x++;
+                }
+                if (o_x > 1) {
+                    return false;
+                }
+                it = new MultiHashMapCellIterator<T>(grid, (int)math.hash(centerGrid + new int2(o_x, o_y)));
+            }
+
+            return true;
+        }
+
+        public T Current => it.Current;
+    }
+
+    private struct MultiHashMapCellIterator<T> where T : struct {
+        private NativeMultiHashMap<int, T> hashMap;
+        NativeMultiHashMapIterator<int> it;
+        private bool found;
+        public T current;
+        private T nextItem;
+        public MultiHashMapCellIterator<T> GetEnumerator() => this;
+
+        public MultiHashMapCellIterator(NativeMultiHashMap<int, T> hashMap, int hash) {
+            this.hashMap = hashMap;
+            
+            found = hashMap.TryGetFirstValue(hash, out nextItem, out it);
+
+            current = default(T);
+        }
+
+        public bool MoveNext() {
+            if (!found) {
+                return false;
+            }
+
+            current = nextItem;
+            found = hashMap.TryGetNextValue(out nextItem, ref it);
+            return true;
+        }
+
+        public T Current => current;
     }
 }
